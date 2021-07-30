f7ba98708a9a20c334776e76b9b67a9c39b06f84
Octavian Popovici <octavianpopovici13@gmail.com>
2021-07-30 18:45:58 +0300

    created get-started button, and tryed to implement animation on truck image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import image from '../../img/Scene.png'
-import logo from '../../img/logo.png'
-import truck from '../../img/mobel.svg'
+import React from 'react';
+import image from '../../img/Scene.png';
+import logo from '../../img/logo.png';
+import truckImg from '../../img/mobel.svg';
+import arrowImg from '../../img/Union.png';
 import './header.scss'
 import Navbar from "../Navbar";
 
@@ -13,9 +14,14 @@ const Header = () => {
                 <img className="background-img" src={image} alt="background-image"/>
                 <img className="logo-img" src={logo} alt="logo"/>
                 <p className="header-text">Your awesome traffic permit consultant.</p>
+                <div className="get-started">
+                    <button className="get-started-button">GET STARTED
+                        <img src={arrowImg} className="get-started-img" alt="arrow"/>
+                    </button>
+                </div>
                 <Navbar className="navbar-items"/>
             </div>
-            <img className="truck-img" src={truck} alt="truck-img"/>
+            <img className="truck-img" src={truckImg} alt="truck-img"/>
         </div>
     )
 }
