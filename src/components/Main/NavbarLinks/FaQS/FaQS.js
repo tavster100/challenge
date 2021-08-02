0c54c6557f56782e4057b8b9489ea6348ba1fa76
Octavian Popovici <octavianpopovici13@gmail.com>
2021-08-02 22:38:45 +0300

    added fakeapi list, created Container component for the Faqs section, wip style

--- src/components/Main/NavbarLinks/FaQS/FaQS.js
+++ src/components/Main/NavbarLinks/FaQS/FaQS.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import coverImg from '../../../../img/Rectangle 9.png';
+import faqsImg from '../../../../img/Faq.png';
+import './faqs.scss';
+import Container from "../../../API/Container";
+import items from "../../../../fakeapi";
 const FaQS = () => {
-    return(
-        <>
 
-        </>
+    return(
+        <div className="faqs-container" id="faqs">
+            <div className="faqs-title-text">
+                <div className="faqs-title">
+                    FAQ
+                </div>
+                <div className="faqs-text">
+                    Questions and Answers on Professional Traffic Permits:
+                </div>
+            </div>
+            <div className="faqs-section">
+                <img className="faqs-background" src={coverImg} alt="cover-img"/>
+                <img className="faqs-image" src={faqsImg} alt="faq-img"/>
+            </div>
+            <Container items={items}/>
+        </div>
     )
 }
 export default FaQS
---
+++ src/fakeapi.js
@@ -0,0 +1,50 @@
+
+const items = [
+  {
+    id:0,
+    value:"What is proffesional traffic permit?",
+    answer:"Traffic permits are a requirement for conducting professional traffic."
+  },
+  {
+    id:1,
+    value:"How to check the traffic condition?",
+    answer:"Something"
+  },
+  {
+    id:2,
+    value: "What are the requirements for a professional traffic permit?",
+    answer: "Something"
+  },
+  {
+    id:3,
+    value: "Are there professional traffic permit training courses at a distance?",
+    answer: "Something"
+  },
+  {
+    id:4,
+    value: "When is a professional traffic permit needed?",
+    answer: "Something"
+  },
+  {
+    id:5,
+    value: "Are there differences between a traffic permit and a professional traffic permit?",
+    answer: "Something"
+  },
+  {
+    id:6,
+    value: "How much does a commercial traffic permit cost for goods?",
+    answer: "Something"
+  },
+  {
+    id:7,
+    value: "How to plug in for the traffic permit test?",
+    answer: "Something"
+  },
+  {
+    id:8,
+    value: "How is the sample for a professional traffic permit booked?",
+    answer: "Something"
+  },
+]
+
+export default items;
