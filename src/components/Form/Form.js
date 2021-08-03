42b64832e7ffb3c44a34d1710fe5e7ec08866b8a
Octavian Popovici <octavianpopovici13@gmail.com>
2021-08-03 10:46:57 +0300

    fix on form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+
+    const {register, handleSubmit} = useForm();
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
     return(
-        <form>
-            <input type="text" placeholder="Your name" name="name"/>
-            <input type="text" placeholder="Your email address" name="email"/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Your name" name="name" {...register('name', { required: true })}/>
+            <input type="text" placeholder="Your email address" name="email" {...register('email', { required: true })}/>
             <input type="submit"/>
         </form>
     )
